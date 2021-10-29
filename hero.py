590a485a711fefd59f170223e5ec471b2a322294
Stanley <stanley.jeong@shopify.com>
2021-10-29 14:27:22 -0700

    added abilities and armor

--- hero.py
+++ hero.py
@@ -6,6 +6,21 @@ class Hero:
     self.starting_health = starting_health
     self.current_health = starting_health
 
+  def add_ability(self, ability):
+    pass
+
+  def attack(self):
+    pass
+
+  def defend(self, incoming_damage):
+    pass
+
+  def take_damage(self, damage):
+    pass
+
+  def is_alive(self):
+    pass
+
   def fight(self, opponent):
     match = [self.name, opponent.name]
     random_index = random.randint(0, 1)
